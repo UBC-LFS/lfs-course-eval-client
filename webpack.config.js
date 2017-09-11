0387c6cc564886d69bbfb9f6c44eaa9c446c6c45
Justin Lee <justinlee0022@gmail.com>
2017-09-11 08:39:38 -0700

    successfully build script right into server

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, '../lfs-course-eval-server/public/scripts')
     },
     module: {
         rules: [{
