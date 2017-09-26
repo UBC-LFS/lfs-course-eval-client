8a0091ff572c6dd722995ee1052382ffdf436a1e
Justin Lee <justin.lee@ubc.ca>
2017-09-25 21:11:50 -0700

    Simplified createFilterString and fixed bug caused by appending to global string

--- src/service/dataService.js
+++ src/service/dataService.js
@@ -1,33 +1,25 @@
+import R from 'ramda'
+
 const fetchJSON = (url) => fetch(url).then(response => response.json())
 
-let filterString = ''
-const createFilterString = (filterSettings) => {
-    const keyArray = Object.keys(filterSettings)
-    for (let i = 0; i < keyArray.length; i++) {
-        var filterValue = filterSettings[keyArray[i]]
-        if (typeof filterValue !== 'object') {
-            filterString = filterString + '/' + filterValue
-        }
-        else { createFilterString(filterValue) }
-    }
-    return filterString
+const createFilterString = ({ time, courseNum, department, toggleBelowMin, questionCode, classSizeMin, classSizeMax }) => {
+    return '/' + [time.year, time.term, courseNum, department, toggleBelowMin, questionCode, classSizeMin, classSizeMax].join('/')
 }
 
 const loadData = (filterSettings = {
-                            time: {
-                                year: '2016',
-                                term: 'W2'
-                            },
-                            courseNum: 'LFSLC 100 001',
-                            department: 'LFS',
-                            toggleBelowMin: true,
-                            questionCode: 'UMI6',
-                            classSizeMin: 0,
-                            classSizeMax: 300 // [min, max]
-                        }, chartKey) => {
-    let url = ''
-    url = 'data/' + chartKey + createFilterString(filterSettings)
-    console.log(url)
+    time: {
+        year: '2016',
+        term: 'W2'
+    },
+    courseNum: 'LFSLC 100 001',
+    department: 'LFS',
+    toggleBelowMin: true,
+    questionCode: 'UMI6',
+    classSizeMin: 0,
+    classSizeMax: 300 // [min, max]
+}, chartKey) => {
+    let url = 'data/' + chartKey + createFilterString(filterSettings)
+    if (chartKey === 'c1') console.log(createFilterString(filterSettings))
     //Temporary filter settings, change to real filters once applied
     //fetchJSON(url).then(x => drawUMIvsDispersion(x))
     return fetchJSON(url)
