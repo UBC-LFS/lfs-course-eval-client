86590e882fec0e4edb05785162b01834369c105b
Justin Lee <justinlee0022@gmail.com>
2017-10-25 13:02:19 -0700

    Fixed styling issue

--- src/viz/drawCoursePerformanceTable.js
+++ src/viz/drawCoursePerformanceTable.js
@@ -26,12 +26,12 @@ const drawCoursePerformance = (tableData = courseData.courses, questionCode) => 
             }).css({
               'width': (row[2] / 5) * 100 + '%'
             }))
-            bars.push($('<div></div>', { 'class': 'line' }).css({ 'left': (row[5] / 5) * 100 + '%' }).css({'color': '#ccc'}).append(function(){
+            bars.push($('<div></div>', { 'class': 'line' }).css({ 'left': (row[5] / 5) * 100 + '%' }).css({'color': '#ccc'}).append(function () {
               return $('<span></span>', {
                 'class': 'tooltiptext'
               }).append('Course Average: ' + row[5]).prop('outerHTML')
             }))
-            bars.push($('<div></div>', { 'class': 'line' }).css({ 'left': (row[6] / 5) * 100 + '%' }).css({'color': 'gold'}).append(function(){
+            bars.push($('<div></div>', { 'class': 'line' }).css({ 'left': (row[6] / 5) * 100 + '%' }).css({'color': 'gold'}).append(function () {
               return $('<span></span>', {
                 'class': 'tooltiptext'
               }).append('Department Average: ' + row[6]).prop('outerHTML')
