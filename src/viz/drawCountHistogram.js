85e4816bb4208a2456ca9e976392ff988c8fab73
Justin Lee <justinlee0022@gmail.com>
2017-10-27 10:53:54 -0700

    Added text and fixed sizing

--- src/viz/drawCountHistogram.js
+++ src/viz/drawCountHistogram.js
@@ -11,7 +11,7 @@ let sampleCount = {
 }
 
 const drawCountHistogram = (count = sampleCount) => {
-  const width = 960 - margin.left - margin.right
+  const width = 500 - margin.left - margin.right
 
   const x = d3.scaleLinear().rangeRound([0, width])
   const y = d3.scaleLinear().range([height, 0])
@@ -26,7 +26,7 @@ const drawCountHistogram = (count = sampleCount) => {
 
   const bins = d3.histogram().thresholds([1, 2, 3, 4, 5])(arrayOfCounts)
 
-  // console.log(arrayOfCounts, bins)
+  console.log(bins)
 
   y.domain([0, d3.max(bins, (d) => d.length)])
 
@@ -34,34 +34,19 @@ const drawCountHistogram = (count = sampleCount) => {
     .data(bins)
     .enter().append('g')
       .attr('class', 'bar')
-      .attr('transform', function (d) { return 'translate(' + x((d.x0 - 1) / 5) + ',' + y(d.length) + ')' })
+      .attr('transform', (d) => 'translate(' + x((d.x0 - 1) / 5) + ',' + y(d.length) + ')')
 
   bar.append('rect')
     .attr('x', 1)
     .attr('width', x(bins[0].x1 / 5) - x(bins[0].x0 / 5) - 1)
-    .attr('height', function (d) {
-      // console.log(d)
-      console.log(height, d.length)
-      return height - y(d.length)
-    })
+    .attr('height', (d) => height - y(d.length))
 
-  // svg.selectAll('rect')
-  //   .data(bins)
-  //   .enter().append('rect')
-  //   .attr('class', 'bar')
-  //   .attr('x', 1)
-  //   .attr('transform', (d) => 'translate(' + x(d.x0) + ',' + y(d.length) + ')')
-  //   .attr('width', (d) => x(d.x1) - x(d.x0) - 1)
-  //   .attr('height', (d) => height - y(d.length))
-
-  const dataT = d3.range(1000).map(d3.randomBates(10))
-
-  var bins2 = d3.histogram()
-    .domain(x.domain())
-    .thresholds(x.ticks(20))
-    (dataT)
-
-  console.log('bins', bins, 'bins2', bins2)
+  bar.append('text')
+    .attr('dy', '.75em')
+    .attr('y', 6)
+    .attr('x', (x(bins[0].x1 / 5) - x(bins[0].x0 / 5)) / 2)
+    .attr('text-anchor', 'middle')
+    .text((d) => 'UMI' + d.x0 + ': ' + d.length)
 }
 
 export default drawCountHistogram
