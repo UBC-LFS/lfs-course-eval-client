2a74102fa8481335c08574c6906f85c39e9c3394
Justin Lee <justinlee0022@gmail.com>
2017-11-14 15:37:39 -0800

    Refactored drawUMIInstructorTable

--- src/viz/drawUMIInstructorTable.js
+++ src/viz/drawUMIInstructorTable.js
@@ -5,7 +5,15 @@ import * as util from '../util/util.js'
 const drawUMIInstructor = (UMIInstructorData, PUID = 'X53VU8MB9D08') => {
   const tableData = R.find(x => x.PUID === PUID, UMIInstructorData).Courses
   const data = tableData.map(x => (
-    [x.course + ' ' + x.section, util.roundToTwoDecimal(x.UMI1.average), util.roundToTwoDecimal(x.UMI2.average), util.roundToTwoDecimal(x.UMI3.average), util.roundToTwoDecimal(x.UMI4.average), util.roundToTwoDecimal(x.UMI5.average), util.roundToTwoDecimal(x.UMI6.average), x.year]
+    [x.course + ' ' + x.section,
+      util.roundToTwoDecimal(x.UMI1.average),
+      util.roundToTwoDecimal(x.UMI2.average),
+      util.roundToTwoDecimal(x.UMI3.average),
+      util.roundToTwoDecimal(x.UMI4.average),
+      util.roundToTwoDecimal(x.UMI5.average),
+      util.roundToTwoDecimal(x.UMI6.average),
+      x.year
+    ]
   ))
   $('#UMIInstructors').DataTable({
     'aaData': data,
