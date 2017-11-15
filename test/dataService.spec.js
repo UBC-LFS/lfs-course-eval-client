9c2929417d6e8cab21d7eecc794da739b0424c36
Justin Lee <justinlee0022@gmail.com>
2017-11-15 13:33:23 -0800

    deleted unused test

--- test/dataService.spec.js
+++ test/dataService.spec.js
@@ -3,26 +3,6 @@
 import * as dataService from '../src/service/dataService'
 import assert from 'assert'
 
-// describe('createFilterString', () => {
-//   it('returns a string with the appropriate paramters', () => {
-//     const data = {
-//       time: {
-//         year: '2016',
-//         term: 'W2'
-//       },
-//       courseNum: 'LFSLC 100 001',
-//       department: 'LFS',
-//       toggleBelowMin: true,
-//       questionCode: 'UMI6',
-//       classSizeMin: 0,
-//       classSizeMax: 300 // [min, max]
-//     }
-//     const chartKey = 'c1'
-//     const expected = 'data/c1/2016/W2/LFSLC 100 001/LFS/true/UMI6/0/300'
-//     assert.deepEqual(expected, dataService.createFilterString(data, chartKey))
-//   })
-// })
-
 describe('need to test', () => {
   it('needs to be tested', () => {
 
