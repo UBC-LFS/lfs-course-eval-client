502aa467c5cdd1056eb484a0119bf4536eb3f0ec
clarachu <cgc.chu@gmail.com>
2017-11-28 12:52:05 -0800

    CC - enrolment chart start at 0 for y-axis

--- src/viz/drawEnrolmentLineChart.js
+++ src/viz/drawEnrolmentLineChart.js
@@ -7,38 +7,47 @@ import * as d3 from 'd3'
 const lineMargin = { top: 60, bottom: 50, left: 50, right: 30 }
 
 const createLineChart = (data, course, term) => {
-    const courseData = data.find(x => x.Course === course)
-    const dataset = filterByTerm(term, sortByTerm(courseData.Terms))
-    const terms = []
-    const enrolment = []
-    dataset.map(x => { terms.push(x.year) 
-        enrolment.push(x.enrolment) })
+  const courseData = data.find(x => x.Course === course)
+  const dataset = filterByTerm(term, sortByTerm(courseData.Terms))
+  const terms = []
+  const enrolment = []
+  dataset.map(x => {
+    terms.push(x.year)
+    enrolment.push(x.enrolment)
+  })
 
-    const dataNew = {
-        labels: terms,
-        datasets: [
-            {
-                label: "Enrolment",
-                lineTension: 0,
-                backgroundColor: "rgba(151,187,205,0.2)",
-                borderColor: "rgba(151,187,205,1)",
-                pointBackgroundColor: "rgba(151,187,205,1)",
-                pointRadius: 5,
-                pointBorderColor: "#fff",
-                pointHoverBackgroundColor: "#fff",
-                pointHoverBorderColor: "rgba(151,187,205,1)",
-                data: enrolment
-            }
-        ]
+  const dataNew = {
+    labels: terms,
+    datasets: [
+      {
+        label: 'Enrolment',
+        lineTension: 0,
+        backgroundColor: 'rgba(151,187,205,0.2)',
+        borderColor: 'rgba(151,187,205,1)',
+        pointBackgroundColor: 'rgba(151,187,205,1)',
+        pointRadius: 5,
+        pointBorderColor: '#fff',
+        pointHoverBackgroundColor: '#fff',
+        pointHoverBorderColor: 'rgba(151,187,205,1)',
+        data: enrolment
+      }
+    ]
+  }
+  const ctx = document.getElementById('enrolmentLineChartCanvas').getContext('2d')
+  const options = {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
+        }
+      }]
     }
-    var ctx = document.getElementById("enrolmentLineChartCanvas").getContext("2d");
-    var options = {};
-    var lineChart = new Chart(ctx,{
-        type: 'line',
-        data: dataNew,
-        options: options
-    })
+  }
+  const lineChart = new Chart(ctx, {
+    type: 'line',
+    data: dataNew,
+    options: options
+  })
 }
-
 
 export default createLineChart
