acd2e8ac7fa4a98d1d73f96fb7fbbe2000a204fe
Justin Lee <justinlee0022@gmail.com>
2017-12-15 12:47:29 -0800

    Added comparison from previous year

--- src/controller/overviewController.js
+++ src/controller/overviewController.js
@@ -1,5 +1,7 @@
 import { loadOverview } from '../service/dataService'
 
+const toTwoDecimal = decimal => Math.round(decimal * 100) / 100
+
 const initOverview = () => {
   const year = 2016
   loadOverview(year).then(data => {
@@ -8,13 +10,18 @@ const initOverview = () => {
     const enrolment = document.getElementById('card-enrolment')
     const responseRate = document.getElementById('card-response-rate')
     const sections = document.getElementById('card-number-of-sections')
-    const title = document.getElementById('card-title')
 
-    umi.innerHTML = data.currentYear.umi6
-    enrolment.innerHTML = data.currentYear.enrolment
-    responseRate.innerHTML = data.currentYear.responseRate
-    sections.innerHTML = data.currentYear.sections
+    const title = document.getElementById('card-title')
     title.innerHTML = year + ' Overview'
+
+    const elements = [umi, enrolment, responseRate, sections]
+    const currentYear = [data.currentYear.umi6, data.currentYear.enrolment, data.currentYear.responseRate, data.currentYear.sections]
+    const previousYear = [data.previousYear.umi6, data.previousYear.enrolment, data.previousYear.responseRate, data.previousYear.sections]
+
+    elements.map((element, i) => (element.innerHTML = currentYear[i]))
+
+    const comparisons = document.getElementsByClassName('card-comparison')
+    Array.from(comparisons).map((x, i) => (x.innerHTML = (toTwoDecimal(currentYear[i] / previousYear[i] * 100 - 100) + '%')))
   })
 }
 
