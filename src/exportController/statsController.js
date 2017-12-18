7580bbb0137e937ad966d3b8a1e2bd5ea7b9ac1b
Justin Lee <justinlee0022@gmail.com>
2017-12-18 10:40:28 -0800

    values from export inputs are being sent to server

--- src/exportController/statsController.js
+++ src/exportController/statsController.js
@@ -1,12 +1,15 @@
 import { loadStats } from '../service/exportDataService'
 
 const statsController = () => {
-  const fromYear = document.getElementById('export-from-year')
-  const toYear = document.getElementById('export-to-year')
-  const dept = document.getElementById('export-dept')
-
-  document.getElementById('export-button').onclick(() => {
-    console.log(fromYear.value)
+  document.getElementById('export-button').addEventListener('click', function () {
+    const fromYear = document.getElementById('export-from-year').value
+    const toYear = document.getElementById('export-to-year').value
+    const dept = document.getElementById('export-dept').value
+    loadStats({
+      fromYear,
+      toYear,
+      dept
+    })
   })
 } 
 
