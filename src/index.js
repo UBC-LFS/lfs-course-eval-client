401bd2fcac769075102b1d6ebbb42cbbec95998b
Justin Lee <justinlee0022@gmail.com>
2017-12-18 09:51:29 -0800

    calling initExportView() when on export view page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 /* global $ */
 import initOverview from './overviewController/initOverview'
+import initExportView from './exportController/initExportView'
 
 document.addEventListener('DOMContentLoaded', function () {
   $('.selectpicker').selectpicker()
@@ -10,5 +11,5 @@ document.addEventListener('DOMContentLoaded', function () {
   if (window.location.pathname === '/') initOverview()
   if (window.location.pathname === '/course') console.log('on course')
   if (window.location.pathname === '/instructor') console.log('on instructor')
-  if (window.location.pathname === '/export') console.log('on export')
+  if (window.location.pathname === '/export') initExportView()
 }, false)
