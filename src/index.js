d9261442cce6097bf1a3f3672ca4417e367fb725
Justin Lee <justinlee0022@gmail.com>
2017-12-18 09:45:37 -0800

    adding js routing handling

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ document.addEventListener('DOMContentLoaded', function () {
   $('.bootstrap-select').click(function () {
     $(this).addClass('open')
   })
-
-  // call the right code depending on which page we're on
-  initOverview()
+  // loading correct JS file on route
+  if (window.location.pathname === '/') initOverview()
+  if (window.location.pathname === '/course') console.log('on course')
+  if (window.location.pathname === '/instructor') console.log('on instructor')
+  if (window.location.pathname === '/export') console.log('on export')
 }, false)
