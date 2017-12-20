04078acde21d8eac45de8d335fc124316501ca4b
Justin Lee <justinlee0022@gmail.com>
2017-12-20 10:35:45 -0800

    Styling changes

--- src/instructorCourseViewController/OverallInstructor/drawOverallInstructorTable.js
+++ src/instructorCourseViewController/OverallInstructor/drawOverallInstructorTable.js
@@ -2,19 +2,20 @@
 import { stripMiddleName } from '../../util/util'
 
 const drawOverallInstructor = tableData => {
-  const data = tableData.map(x => (
-    [stripMiddleName(x.instructorName),
-      x.dept,
-      x.UMI1.average,
-      x.UMI2.average,
-      x.UMI3.average,
-      x.UMI4.average,
-      x.UMI5.average,
-      x.UMI6.average,
-      Math.round(x.UMI6.percentFavourable * 100) + '%',
-      x.numCoursesTaught,
-      x.numStudentsTaught]
-  ))
+  const data = tableData.map(x => ([
+    stripMiddleName(x.instructorName),
+    x.dept,
+    x.UMI1.average,
+    x.UMI2.average,
+    x.UMI3.average,
+    x.UMI4.average,
+    x.UMI5.average,
+    x.UMI6.average,
+    Math.round(x.UMI6.percentFavourable * 100) + '%',
+    x.numCoursesTaught,
+    x.numStudentsTaught
+  ])
+  )
 
   $('#OverallInstructors').DataTable({
     'aaData': data,
