f3c0024fff1fea96a5bb4dd5a97d3ca9d277db16
Justin Lee <justinlee0022@gmail.com>
2018-01-16 09:29:53 -0800

    removed console

--- src/controllers/overviewController/Dashboard/overviewController.js
+++ src/controllers/overviewController/Dashboard/overviewController.js
@@ -5,7 +5,6 @@ const toTwoDecimal = decimal => Math.round(decimal * 100) / 100
 const initHighLevelOverview = () => {
   const year = 2016
   loadOverview(year).then(data => {
-    console.log('highLevelOverview data', data)
     const umi = document.getElementById('card-umi')
     const enrolment = document.getElementById('card-enrolment')
     const responseRate = document.getElementById('card-response-rate')
