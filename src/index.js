8a0febc0fd17f7f6b2046439abc2500dea8670d7
Justin Lee <justinlee0022@gmail.com>
2018-01-25 08:28:58 -0800

    Updated dependencies and removed unused web path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 /* global $ */
 import initOverview from './controllers/overviewController/initOverview'
-import initInstructorCourseView from './controllers/instructorCourseViewController/initInstructorCourseView'
 import initInstructorView from './controllers/instructorViewController/initInstructorView'
 import initExportView from './controllers/exportController/initExportView'
 
@@ -10,7 +9,6 @@ document.addEventListener('DOMContentLoaded', function () {
     $(this).addClass('open')
   })
   if (window.location.pathname === '/') initOverview()
-  if (window.location.pathname === '/course') initInstructorCourseView()
   if (window.location.pathname === '/instructor') initInstructorView()
   if (window.location.pathname === '/export') initExportView()
 }, false)
