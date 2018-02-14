f6bbba5b609fc0c13ce90d4d92a4f6d1cc87fb0c
Justin Lee <justinlee0022@gmail.com>
2018-02-14 08:38:34 -0800

    Added active class to current tab in bootstrap

--- src/index.js
+++ src/index.js
@@ -8,7 +8,24 @@ document.addEventListener('DOMContentLoaded', function () {
   $('.bootstrap-select').click(function () {
     $(this).addClass('open')
   })
-  if (window.location.pathname === '/') initOverview()
-  if (window.location.pathname === '/instructor') initInstructorView()
-  if (window.location.pathname === '/export') initExportView()
+
+  const navbar = document.getElementsByClassName('navbar-nav')[0]
+  if (window.location.pathname === '/') {
+    navbar.children[0].className = 'active'
+    navbar.children[1].className = ''
+    navbar.children[2].className = ''
+    initOverview()
+  }
+  if (window.location.pathname === '/instructor') {
+    navbar.children[0].className = ''
+    navbar.children[1].className = 'active'
+    navbar.children[2].className = ''
+    initInstructorView()
+  }
+  if (window.location.pathname === '/export') {
+    navbar.children[0].className = ''
+    navbar.children[1].className = ''
+    navbar.children[2].className = 'active'
+    initExportView()
+  }
 }, false)
